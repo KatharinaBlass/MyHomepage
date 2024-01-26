7c1fff1b3a43155d10b9b28e0906d2b3a45ec81e
Kathi <katharina.blass@student.hpi.de>
2024-01-26 17:04:30 +0100

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 import styled from 'styled-components'
 import profile from './profile.jpg'
-import './App.css';
+import './App.css'
 
 const AppContent = styled.div`
   padding: 4%;
@@ -12,31 +12,26 @@ const AppContent = styled.div`
   justify-content: center;
 `
 const Profile = styled.img`
-    height: 60vh;
-    border-radius: 50%;
-    border: .25rem solid black;
-    padding: 1%;
-    margin-right: 4%;
-  `
-
-
+  height: 60vh;
+  border-radius: 50%;
+  border: 0.25rem solid black;
+  padding: 1%;
+  margin-right: 4%;
+`
 const TextContainer = styled.div`
-    max-width: 40%;
-  `
-
+  max-width: 40%;
+`
 const NameText = styled.h1`
-    font-size: 4rem;
-    font-weight: bold;
-  `
-
+  font-size: 4rem;
+  font-weight: bold;
+`
 const ProfessionText = styled.h2`
-    font-size: 2rem;
-    font-weight: bold;
-  `
-
+  font-size: 2rem;
+  font-weight: bold;
+`
 const ProfileText = styled.p`
-    font-size: 1.25rem;
-  `
+  font-size: 1.25rem;
+`
 
 function App() {
   return (
@@ -45,10 +40,14 @@ function App() {
       <TextContainer>
         <NameText>Hi, I'm Katharina</NameText>
         <ProfessionText>A Frontend Developer</ProfessionText>
-        <ProfileText>I love building modern front-end and mobile applications with fun and curiosity. My passion for UX design and accessibility ensures that the products I create are intuitive and accessible to all.</ProfileText>
+        <ProfileText>
+          I love building modern front-end and mobile applications with fun and
+          curiosity. My passion for UX design and accessibility ensures that the
+          products I create are intuitive and accessible to all.
+        </ProfileText>
       </TextContainer>
     </AppContent>
-  );
+  )
 }
 
-export default App;
+export default App
