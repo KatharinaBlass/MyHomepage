951d2ee8e2f07ee506cc7f63c93b2826b3e88c6b
Kathi <katharina.blass@student.hpi.de>
2024-01-29 13:23:39 +0100

    add background image for later dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
 import profile from './profile.jpg'
+import BackgroundSVGDark from './background-dark.svg'
 import './App.css'
 
 const AppContent = styled.div`
   padding: 4%;
   min-height: 100vh;
-  background-color: white;
   display: flex;
   align-items: center;
   justify-content: center;
+  background-color: #040d21;
+  background-image: url(${BackgroundSVGDark});
+  background-size: cover;
 `
 const Profile = styled.img`
   height: 60vh;
   border-radius: 50%;
-  border: 0.25rem solid black;
+  border: 0.25rem solid #33a8c5;
   padding: 1%;
   margin-right: 4%;
 `
 const TextContainer = styled.div`
   max-width: 40%;
+  color: white;
 `
 const NameText = styled.h1`
   font-size: 4rem;
