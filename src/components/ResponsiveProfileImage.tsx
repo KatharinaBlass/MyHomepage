3969ccbfa6d3868620196aa2466f3504939408ca
Kathi <katharina.blass@student.hpi.de>
2024-02-07 19:25:27 +0100

    improve browser responsive image sizes

--- src/components/ResponsiveProfileImage.tsx
+++ src/components/ResponsiveProfileImage.tsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components'
+import PNG1600 from '../graphics/profile/profile-1600.png'
 import PNG1200 from '../graphics/profile/profile-1200.png'
 import PNG900 from '../graphics/profile/profile-900.png'
 import PNG450 from '../graphics/profile/profile-450.png'
+import AVIF1600 from '../graphics/profile/profile-1600.avif'
 import AVIF1200 from '../graphics/profile/profile-1200.avif'
 import AVIF900 from '../graphics/profile/profile-900.avif'
 import AVIF450 from '../graphics/profile/profile-450.avif'
@@ -38,16 +40,16 @@ export default function ResponsiveProfileImage() {
     <Profile>
       <source
         type="image/avif"
-        srcSet={`${AVIF450} 450w, ${AVIF900} 900w, ${AVIF1200} 1200w`}
-        sizes="(max-width: 450px) 450px, (max-width: 1450px) 900px, 1200px"
-        //load medium sized image (900w) also until max-width of 1450px because of 2-column layout when width>900px
+        srcSet={`${AVIF450} 450w, ${AVIF900} 900w, ${AVIF1200} 1200w, ${AVIF1600} 1600w`}
+        sizes="(max-width: 900px) 100vw, 50vw"
+        //sizes -> have single column layout for widths<=900px and 2-columns above, thus 50vw remain for image
       />
       <source
         type="image/png"
-        srcSet={`${PNG450} 450w, ${PNG900} 900w, ${PNG1200} 1200w`}
-        sizes="(max-width: 450px) 450px, (max-width: 1450px) 900px, 1200px"
+        srcSet={`${PNG450} 450w, ${PNG900} 900w, ${PNG1200} 1200w, ${PNG1600} 1600w`}
+        sizes="(max-width: 450px) 450px, (max-width: 1800px) 900px, 1200px"
       />
-      <Image src={PNG1200} alt="This is my smile for you" />
+      <Image src={PNG900} alt="This is my smile for you" />
     </Profile>
   )
 }
