27dfbe9dcc9259bddf1d201c783a0e549af3c69e
Kathi <katharina.blass@student.hpi.de>
2024-03-01 16:28:46 +0100

    add date range and type to project card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,10 +2,20 @@ import styled from 'styled-components'
 import Chip from './Chip'
 import mockImg from '../graphics/profile/profile-450.png'
 
+export enum ProjectType {
+  design = 'Design',
+  mobile = 'Mobile',
+  game = 'Game',
+  web = 'Web',
+}
+
 export type Project = {
   title: string
   description: string
+  typeDeclarations: ProjectType[]
   techStack: string[]
+  startDate: Date
+  endDate: Date
   githubLink?: string
   deployLink?: string
 }
@@ -15,11 +25,12 @@ const Card = styled.div`
   border: 1px solid ${(props) => props.theme.colors.text};
   border-radius: 8px;
   box-shadow: 6px 6px 0px 0px ${(props) => `${props.theme.colors.primary}55`};
-  max-width: 600px;
+  max-width: 800px;
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 100%;
+  margin: ${(props) => props.theme.spacings.m};
 
   @media (min-width: 600px) {
     flex-direction: row;
@@ -47,17 +58,41 @@ const Title = styled.h2`
   font-weight: normal;
   margin: 0 0 ${(props) => props.theme.spacings.m} 0;
 `
+const Subtitle = styled.p`
+  font-size: ${(props) => props.theme.fontSizes.s};
+  margin: 0 0 ${(props) => props.theme.spacings.m} 0;
+`
+const ProjectTypeInfo = styled.span`
+  font-size: inherit;
+  font-weight: bold;
+  color: ${(props) => props.theme.colors.text};
+  background-color: ${(props) => `${props.theme.colors.primary}33`};
+`
 const Description = styled.p`
   margin: 0 0 ${(props) => props.theme.spacings.m} 0;
 `
 const TechContainer = styled.div``
 
 export default function ProjectCard(props: Project) {
+  const dateFormatOptions: Intl.DateTimeFormatOptions = {
+    month: 'short',
+    year: 'numeric',
+  }
+
   return (
     <Card>
       <ProjectImage src={mockImg} alt=""></ProjectImage>
       <TextContainer>
         <Title>{props.title}</Title>
+        <Subtitle>
+          <ProjectTypeInfo>
+            {props.typeDeclarations.join(' & ')}
+          </ProjectTypeInfo>
+          {' | ' +
+            props.startDate.toLocaleString('en-US', dateFormatOptions) +
+            ' - ' +
+            props.endDate.toLocaleString('en-US', dateFormatOptions)}
+        </Subtitle>
         <Description>{props.description}</Description>
         <TechContainer>
           {props.techStack.map((techItem) => (
--- src/page/Projects.tsx
+++ src/page/Projects.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import ProjectCard, { Project } from '../components/ProjectCard'
+import ProjectCard, { Project, ProjectType } from '../components/ProjectCard'
 
 const ProjectsContent = styled.div`
   display: flex;
@@ -12,13 +12,17 @@ const ProjectsContent = styled.div`
 const SectionTitle = styled.h1`
   font-size: ${(props) => props.theme.fontSizes.xxl};
 `
+const ProjectLayoutContainer = styled.div``
 
 const allProjects: Project[] = [
   {
-    title: 'Accessible everGuide',
+    title: 'Accessible everGuide app',
     description:
       'Im Rahmen meiner Masterarbeit habe ich in Zusammenarbeit mit dem Fraunhofer Institut eine barrierefreie Version ihrer  Indoor-Navigations-Android-App everGuide entwickelt und mit blinden Nutzern getestet.',
+    typeDeclarations: [ProjectType.design, ProjectType.mobile],
     techStack: ['Kotlin', 'WCAG 2.2', 'user testing', 'Figma'],
+    startDate: new Date(2023, 3, 1),
+    endDate: new Date(2023, 10, 23),
   },
 ]
 
@@ -26,9 +30,11 @@ export default function Projects({ id }: { id: string }) {
   return (
     <ProjectsContent id={id}>
       <SectionTitle>Projects</SectionTitle>
-      {allProjects.map((project) => (
-        <ProjectCard {...project} key={project.title}></ProjectCard>
-      ))}
+      <ProjectLayoutContainer>
+        {allProjects.map((project) => (
+          <ProjectCard {...project} key={project.title}></ProjectCard>
+        ))}
+      </ProjectLayoutContainer>
     </ProjectsContent>
   )
 }
