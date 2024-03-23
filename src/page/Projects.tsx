abc5cdb1eae74f56a927645b5fd046003e965d7e
Kathi <katharina.blass@student.hpi.de>
2024-03-23 16:58:08 +0100

    add second project wothout images

--- src/page/Projects.tsx
+++ src/page/Projects.tsx
@@ -18,11 +18,22 @@ const allProjects: Project[] = [
   {
     title: 'Accessible everGuide app',
     description:
-      "As part of my Master's thesis, I worked with the Fraunhofer Institute to develop an accessible version of their indoor navigation app everGuide for Android. This involved optimising for assistive technologies, applying the WCAG 2.2 standard at level AA+, and conducting interviews and tests with visually impaired users.",
+      "As part of my Master's thesis, I worked with the Fraunhofer Institute (FOKUS) to develop an accessible version of their indoor navigation app everGuide for Android. This involved optimising for assistive technologies, applying the WCAG 2.2 standard at level AA+, and conducting interviews and tests with visually impaired users.",
     typeDeclarations: [ProjectType.design, ProjectType.mobile],
     techStack: ['Kotlin', 'WCAG 2.2', 'UX Research', 'Figma'],
     startDate: new Date(2023, 3, 1),
     endDate: new Date(2023, 10, 23),
+    imageFolderName: 'everGuide',
+    imageAltText: '',
+  },
+  {
+    title: 'Dinner planning app',
+    description:
+      "During my Master's studies at the HTW, a fellow student and I developed a mobile application for Android and iOS that facilitates the planning of dinner parties with friends. The app offers suitable recipes for a group consensus, taking into account all dietary preferences and allergies of the participants. My responsibilities were the concept and design of the app as well as the implementation of the front-end for Android.",
+    typeDeclarations: [ProjectType.design, ProjectType.mobile],
+    techStack: ['React Native', 'Google Firebase', 'Figma'],
+    startDate: new Date(2022, 9, 1),
+    endDate: new Date(2023, 2, 31),
     imageFolderName: 'everGuide',
     imageAltText: '',
   },
